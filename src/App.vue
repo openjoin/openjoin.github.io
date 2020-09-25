da409c4e6ceaca6dea7f10072c84c6033de6af28
Katheesh <katheeskumar@outlook.com>
2020-09-25 21:01:54 +0530

    added all files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Home Image" src="./assets/home.svg" height="250" />
+  <HelloWorld msg="We believe Open Source is for everyone, yes you!" />
+  <h2>OPEN SOURCE is NOT about code, it is about people and collaboration</h2>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
