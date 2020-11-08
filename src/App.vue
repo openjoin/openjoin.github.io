f21b33d37c8baabb59c492c19f57e70969c23dd9
Katheesh <katheeskumar@outlook.com>
2020-11-08 19:48:36 +0530

    added new style webpage

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,48 @@
 <template>
-  <div id="app">
-<nav class="navbar is-fixed-top is-danger" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://openjoin.github.io">
-      <img src="/assets/newlogo.png" width="112" height="28">
-    </a>
+  <div>
+    <b-navbar class="navbar docs-navbar is-spaced has-shadow">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="assets/newlogo.png"
+                    alt="OpenJoin"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item>
+              <router-link to="/" class="is-danger">Home</router-link>
+            </b-navbar-item>
+            <b-navbar-item>
+                <router-link to="/about">About</router-link>
+            </b-navbar-item>
+            <b-navbar-item>
+                Contact
+            </b-navbar-item>
+        </template>
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-end">
-      <a class="navbar-item">
-        Home
-      </a>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
-        <a class="navbar-item">
-        How to Join
-        </a>
-    </div>
-  </div>
-</nav>
-    <router-view />
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a href="https://github.com/openjoin/support/issues" class="button is-danger">
+                        <strong>Join with us</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped lang="scss">
+.top-bar {
+    width: 100%;
+    height: 2em;
+    background-color: gray;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.navbar {
+    padding-left: 2rem;
+    padding-right: 2rem;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,93 @@
 <template>
-  <div class="home">
-  	<section class="hero is-medium is-bold">
-       <div class="hero-body">
-        <div class="container">
-        	<div class="columns">
-                <div class="column is-two-thirds">
-                   <img alt="Home Image" src="/assets/home.png"/>
-                </div>
-                <div class="column">
-                <br><br><br>
-
-
-<div class="card is-dark">
-  <div class="card-content">
-    <h1 class="title">
-        OPEN JOIN
-    </h1>
-    <h2 class="subtitle">
-        Open Source is for everyone, yes you! It's not about code, it is about people and collaboration
-    </h2>
-  </div>
-</div>
-                </div>
+   <div>
+      <section class="home-top">
+         <div class="container">
+            <div class="columns">
+               <div class="column">
+                  <b-image
+                     src="assets/home.png"
+                     class="home-img"
+                     ></b-image>
+               </div>
+               <div class="column">
+                  <center>
+                     <h1>Open Source is for everyone, yes you!</h1>
+                     <p><span class="special">OPEN SOURCE</span> is not about code, it is about people and collaboration</p>
+                  </center>
+               </div>
             </div>
-        </div>
-       </div>
-    </section>
-  </div>
+         </div>
+      </section>
+      <section>
+        <div class="container">
+            <div class="columns">
+               <div class="column">
+                  <div class="box">
+                     <article class="media">
+                        <div class="media-left">
+                           <figure class="image is-64x64">
+                              <b-icon
+                                 icon="target"
+                                 size="is-large"
+                                 type="is-danger">
+                              </b-icon>
+                           </figure>
+                        </div>
+                        <div class="media-content">
+                           <div class="content">
+                              <h4> Our Target</h4>
+                              <p>
+                                 We are invested in getting you those green squares 🟩 on GitHub! Contributing to Open Source Software (OSS) is a great way for you to learn, work with different people and also network.
+                              </p>
+                           </div>
+                        </div>
+                     </article>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <Footer/>
+   </div>
 </template>
+<script>
+   import Footer from '@/components/Footer.vue'
+   
+   export default {
+     name: 'Home',
+     data() {
+       return {
+           rounded: false,
+       }
+     },
+     components: {
+       Footer
+     }
+   }
+</script>
+<style scoped>
+   h1 {
+    font-family: 'Lobster', cursive;
+    margin-top: 3em;
+    font-size : 50px;
+   }
+   h4{
+    font-family: 'Lobster', cursive;
+    margin-top: 1em;
+   }
+   p {
+    font-family: 'Kaushan Script', cursive;
+   }
+   .home-img {
+    margin-top: 4em;
+   }
+   .home-top {
+     margin-bottom: 3em;
+   }
+   .special {
+     padding: 2px; 
+     background-color: #ffcfe2;
+     color: maroon;
+     font-family: 'Kaushan Script', cursive;
+   }
+</style>
