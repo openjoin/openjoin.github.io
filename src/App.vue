07bff17bfc1ae194931094dfe4bb91cfeae3a75b
Katheesh <katheeskumar@outlook.com>
2020-11-08 18:55:18 +0530

    added new style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <template>
   <div id="app">
+<nav class="navbar is-fixed-top is-danger" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://openjoin.github.io">
+      <img src="/assets/newlogo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-end">
+      <a class="navbar-item">
+        Home
+      </a>
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+        <a class="navbar-item">
+        How to Join
+        </a>
+    </div>
+  </div>
+</nav>
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="home">
-        <img alt="Home Image" src="/assets/home.svg" height="250" />
-        <HelloWorld msg="Open Source is for everyone, yes you!" />
-        <h2>OPEN SOURCE is not about code, it is about people and collaboration</h2>
+  	<section class="hero is-medium is-bold">
+       <div class="hero-body">
+        <div class="container">
+        	<div class="columns">
+                <div class="column is-two-thirds">
+                   <img alt="Home Image" src="/assets/home.png"/>
+                </div>
+                <div class="column">
+                <br><br><br>
+
+
+<div class="card is-dark">
+  <div class="card-content">
+    <h1 class="title">
+        OPEN JOIN
+    </h1>
+    <h2 class="subtitle">
+        Open Source is for everyone, yes you! It's not about code, it is about people and collaboration
+    </h2>
+  </div>
+</div>
+                </div>
+            </div>
+        </div>
+       </div>
+    </section>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
